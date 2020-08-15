a4b49ec13087f1018baf7276d11cbe2643823c26
David P <61435556+PerezDC@users.noreply.github.com>
2020-08-15 16:21:54 -0700

    Add project files.

---
+++ MilitaryUnits/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MilitaryUnits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("EX 7A - C# - Military Unit (Implementing Inheritance)");
+        }
+    }
+}
---
+++ MilitaryUnits/Vehicles/ATV.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MilitaryUnits
+{
+    class ATV : Vehicles
+    {
+    }
+}
